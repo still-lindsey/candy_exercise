d89f876987a28e0e7962ae078fd07e8b6097474c
Lindsey Still <lindseymstill@gmail.com>
2022-03-23 04:24:25 +0900

    added tests and functions

--- candy_problem/main.py
+++ candy_problem/main.py
@@ -35,17 +35,69 @@ write tests to handle edge cases.
 '''
 
 #1
-def get_friends_favorite_candy_count(favorites):
-    pass
+from hashlib import new
 
+
+def get_friends_most_favored_candy(favorites):
+    candy_dict = {}
+
+    for friend in favorites:
+        for candy in friend[1]:
+            # print(candy)
+            if candy.lower() not in candy_dict:
+                candy_dict[candy] = 1
+            elif candy in candy_dict:
+                candy_dict[candy] += 1
+
+    return candy_dict
+
+friend_favorites = [
+    ["Sally", ["lollipop", "bubble gum", "laffy taffy" ]],
+    ["Bob", ["milky way", "licorice", "lollipop" ]],
+    ["Arlene", ["chocolate bar", "milky way", "laffy taffy" ]],
+    ["Carlie", ["nerds", "sour patch kids", "laffy taffy" ]]
+]
 #2
-def create_new_candy_data_structure(data):
-    pass 
+def create_new_candy_data_structure(favorites):
+
+    new_candy = {}
+
+    for friend in favorites:
+        for candy in friend[1]:
+
+            if candy in new_candy:
+                new_candy[candy].append(friend[0])
+            else:
+                new_candy[candy]= [friend[0]]
+                
+    print(new_candy)    
+    return new_candy
+
+create_new_candy_data_structure(friend_favorites)
+
+
 
 #3
 def get_friends_who_like_specific_candy(data, candy_name):
-    pass
+
+    if candy_name in data:
+
+        friends_who_like_candy = tuple(data[candy_name])
+    else:
+        return None
+    
+    return friends_who_like_candy
+
 
 #4
 def create_candy_set(data):
-    pass 
+
+    set_of_candies = set()
+
+    for candy in data:
+        set_of_candies.add(candy)
+
+    print(set_of_candies)
+    return set_of_candies
+
+#5 write tests
--- tests/test_candy_data_structure.py
+++ tests/test_candy_data_structure.py
@@ -2,10 +2,36 @@ import pytest
 from candy_problem.main import * 
 
 
+def test_get_friends_most_favorite_candy():
+    data = [
+    ["Sally", ["lollipop", "bubble gum", "laffy taffy" ]],
+    ["Bob", ["milky way", "licorice", "lollipop" ]],
+    ["Arlene", ["chocolate bar", "milky way", "laffy taffy" ]],
+    ["Carlie", ["nerds", "sour patch kids", "laffy taffy" ]]
+]
+
+    candy_dict = get_friends_most_favored_candy(data)
+
+    assert candy_dict["chocolate bar"] == 1
+    assert candy_dict["laffy taffy"] == 3
+
+def test_get_friends_most_favorite_candy_capitalized():
+    data = [
+    ["Lollipop", "bubble gum", "laffy taffy" ],
+    ["Bob", ["milky way", "licorice", "lollipop" ]],
+    ["Arlene", ["chocolate bar", "milky way", "laffy taffy" ]],
+    ["Carlie", ["nerds", "sour patch kids", "laffy taffy" ]]
+]
+
+    candy_dict = get_friends_most_favored_candy(data)
+
+    assert candy_dict["chocolate bar"] == 1
+    
+
 def test_create_candy_data_structure_type():
     # Arrange
     friend_favorites = [
-        ["Sally", [ "lollipop”, “bubble gum", "laffy taffy"]],
+        ["Sally", [ "lollipop", "bubble gum", "laffy taffy"]],
         [ "Bob", ["milky way", "licorice", "lollipop"]],
         [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
         [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
@@ -22,7 +48,7 @@ def test_create_candy_data_structure_values():
 
     # Arrange
     friend_favorites = [
-        ["Sally", [ "lollipop”, “bubble gum", "laffy taffy"]],
+        ["Sally", [ "lollipop", "bubble gum", "laffy taffy"]],
         [ "Bob", ["milky way", "licorice", "lollipop"]],
         [ "Arlene", ["chocolate bar", "milky way", "laffy taffy"]],
         [ "Carlie", ["nerds", "sour patch kids", "laffy taffy"]]
@@ -32,4 +58,69 @@ def test_create_candy_data_structure_values():
     new_candy_data = create_new_candy_data_structure(friend_favorites)
     
     # Assert
-    
+    assert len(new_candy_data) == 8
+    assert new_candy_data == {'lollipop': ['Sally', 'Bob'], 'bubble gum': ['Sally'], 'laffy taffy': ['Sally', 'Arlene', 'Carlie'], 'milky way': ['Bob', 'Arlene'], 'licorice': ['Bob'], 'chocolate bar': ['Arlene'], 'nerds': ['Carlie'], 'sour patch kids': ['Carlie']}
+
+def test_get_friends_who_like_lollipop():
+    #arrange
+
+
+    data = {'lollipop': ['Sally', 'Bob'], 'bubble gum': ['Sally'], 'laffy taffy': ['Sally', 'Arlene', 'Carlie'], 'milky way': ['Bob', 'Arlene'], 'licorice': ['Bob'], 'chocolate bar': ['Arlene'], 'nerds': ['Carlie'], 'sour patch kids': ['Carlie']}
+
+    candy_name = "lollipop"
+
+
+    #act
+    friends_who_like_specific_candy = get_friends_who_like_specific_candy(data, candy_name)
+
+    #assert
+    assert friends_who_like_specific_candy == ("Sally", "Bob")
+
+def test_get_friends_who_like_laffy_taffy():
+    #arrange
+
+
+    data = {'lollipop': ['Sally', 'Bob'], 'bubble gum': ['Sally'], 'laffy taffy': ['Sally', 'Arlene', 'Carlie'], 'milky way': ['Bob', 'Arlene'], 'licorice': ['Bob'], 'chocolate bar': ['Arlene'], 'nerds': ['Carlie'], 'sour patch kids': ['Carlie']}
+
+    candy_name = "laffy taffy"
+
+
+    #act
+    friends_who_like_specific_candy = get_friends_who_like_specific_candy(data, candy_name)
+
+    #assert
+    assert friends_who_like_specific_candy == ("Sally", "Arlene", "Carlie")
+    assert len(friends_who_like_specific_candy) == 3
+
+def test_get_friends_who_like_candy_not_in_data():
+    #arrange
+
+
+    data = {'lollipop': ['Sally', 'Bob'], 'bubble gum': ['Sally'], 'laffy taffy': ['Sally', 'Arlene', 'Carlie'], 'milky way': ['Bob', 'Arlene'], 'licorice': ['Bob'], 'chocolate bar': ['Arlene'], 'nerds': ['Carlie'], 'sour patch kids': ['Carlie']}
+
+    candy_name = "black thunder"
+
+
+    #act
+    friends_who_like_specific_candy = get_friends_who_like_specific_candy(data, candy_name)
+
+    #assert
+    friends_who_like_specific_candy is None
+
+def test_create_candy_set():
+    #arrange
+
+    data = {'lollipop': ['Sally', 'Bob'], 'bubble gum': ['Sally'], 'laffy taffy': ['Sally', 'Arlene', 'Carlie'], 'milky way': ['Bob', 'Arlene'], 'licorice': ['Bob'], 'chocolate bar': ['Arlene'], 'nerds': ['Carlie'], 'sour patch kids': ['Carlie']}
+
+    #act
+    set_of_candies = create_candy_set(data)
+
+    #assert
+    assert len(set_of_candies) == 8
+
+def test_create_candy_set_no_data():
+    data = {}
+
+    set_of_candies = create_candy_set(data)
+
+    assert set_of_candies == set()
